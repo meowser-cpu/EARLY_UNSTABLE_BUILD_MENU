99f9045dcf15a14d1482e4769a6d6c879145693c
meowser-cpu <awesomebubster@gmail.com>
2026-08-18 12:09:22 -0500

    Delete python directory
    Updating code

--- python/filesupport_beta.py
+++
@@ -1,6 +0,0 @@
-import pygame
-from os.path import join
-def import_image(*path, alpha = True, format = 'jpg'):
-    full_path = join(*path) + f'.{format}'
-    surf = pygame.image.load(full_path).convert_alpha() if alpha else pygame.image.load(full_path).convert()
-    return surf
